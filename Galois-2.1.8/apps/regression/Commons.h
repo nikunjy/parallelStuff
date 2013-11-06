a8b59d9c2ddcebdfa67352a3a1a4400685b3e885
nikunjy <nyadav@soho.ices.utexas.edu>
2013-11-06 16:08:45 -0600

    Feature Scaling and Mean Normalization

---
+++ Galois-2.1.8/apps/regression/Commons.h
@@ -0,0 +1,26 @@
+#ifndef COMMONS_H 
+#define COMMONS_H
+#include "Galois/Graphs/Graph.h"
+#include "Galois/Galois.h"
+#include "Galois/Bag.h"
+#include "Galois/Statistic.h"
+#include "Galois/Graphs/LCGraph.h"
+#include "Galois/ParallelSTL/ParallelSTL.h"
+#ifdef GALOIS_USE_EXP
+#include "Galois/Runtime/ParallelWorkInline.h"
+#endif
+#include "llvm/Support/CommandLine.h"
+#include "Lonestar/BoilerPlate.h"
+#include "boost/optional.hpp"
+
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+struct Node { 
+  std::vector <float> featureValues;
+	float outValue; 
+};
+typedef Galois::Graph::FirstGraph<Node,void,false> Graph;  //we should use LC graphs
+typedef Graph::GraphNode GNode;
+#endif
--- Galois-2.1.8/apps/regression/LinearRegression.cpp
+++ Galois-2.1.8/apps/regression/LinearRegression.cpp
@@ -1,13 +1,10 @@
 /** Linear Regression with multiple variables -*- C++ -*-
  * @file
- *
  * Simple Linear regression with multivariables. With things like 
  	1. Mean Normalization 
  	2. Scaling
  	3. Learning rate adaptation
- *
  * @section License
- *
  * Galois, a framework to exploit amorphous data-parallelism in irregular
  * programs.
  *
@@ -26,92 +23,90 @@
  *
  * @author Nikunj Yadav < nikunj@cs.utexas.edu >
  */
-#include "Galois/Graphs/Graph.h"
-#include "Galois/Galois.h"
-#include "Galois/Bag.h"
-#include "Galois/Statistic.h"
-#include "Galois/Graphs/LCGraph.h"
-#include "Galois/ParallelSTL/ParallelSTL.h"
-#ifdef GALOIS_USE_EXP
-#include "Galois/Runtime/ParallelWorkInline.h"
-#endif
-#include "llvm/Support/CommandLine.h"
-
-#include "Lonestar/BoilerPlate.h"
-
-#include "boost/optional.hpp"
-
-#include <utility>
-#include <vector>
-#include <algorithm>
-#include <iostream>
+#include "Commons.h"
 using namespace std;
 
-const char* name = "Maximal Independent Set";
-const char* desc = "Compute a maximal independent set (not maximum) of nodes in a graph";
-const char* url = "independent_set";
+const char* name = "Linear Regression with multivariables";
+const char* desc = "Compute the gradient descent for linear regression with linear variables";
+const char* url = "regression";
 namespace cll = llvm::cl;
 static cll::opt<std::string> filename(cll::Positional, cll::desc("<input file>"), cll::Required);
 
-
-/*
-*Each Node represents an equation
-*the form is y = a1x1 + a2x2 + a3x3+ ... 
-* In the end we want to be able to find out the a1,a2,a3.... 
-*/
-struct Node { 
-	vector <double> featureValues;
-	double outValue;
-
-};
-vector<double> globalThetas;
-typedef Galois::Graph::FirstGraph<Node,void,false> Graph;  //we should use LC graphs
-typedef Graph::GraphNode GNode;
-int featureSize;
+vector<float> globalThetas;
 Graph graph;
-void readGraph(string fileName) { 
+void readGraph(string fileName,int &featureSize,int &numSamples) { 
 	fstream fin(fileName.c_str(),ios::in);
 	vector<Node> equations;
 	int dim,V; 
 	fin>>dim>>V;
 	featureSize = dim;
+  numSamples = V;
+  vector<float> means(dim+1,0);
+  vector<pair<float,float> > variance(dim+1);
 	for ( int i = 0; i < V; i++) { 
 		Node node;
-		for (int k = 0; k < dim ; k++) { 
-			double value; 
+		for (int k = 0; k <= dim ; k++) { 
+			float value; 
 			fin>>value;
-			node.featureValues.push_back(value);
-		}
-		double value; 
-		fin>>value; 
-		node.outValue = value;
-		equations.push_back(node);
-	}
+      means[k] += value;
+      if (i ==0) { 
+        variance[k].first = value;
+        variance[k].second = value;
+      } else { 
+        variance[k].first = (variance[k].first > value) ? variance[k].first : value;
+        variance[k].second = (variance[k].second < value) ? variance[k].second : value; 
+      }
+			if (k != dim) { 
+        node.featureValues.push_back(value); 
+      } else {
+		    node.outValue = value;
+      }
+		  equations.push_back(node);
+	  }
+  }
+  for ( int k = 0; k <= dim ; k++) { 
+    means[k]/=(double)V;
+  }
 	struct fillGraph { 
+    vector<float> &means;
+    vector<pair<float,float> > &var;
+    fillGraph(vector<float> &m, vector<pair<float, float> >&v):
+      means(m),var(v) {
+    }
 		void operator()(Node node) { 
 			GNode gNode = graph.createNode(node);
-			graph.addNode(gNode,Galois::NONE);
+      graph.addNode(gNode,Galois::NONE);
+      auto &nd = graph.getData(gNode,Galois::NONE);
+      vector<float> &fv(nd.featureValues);
+      for (int j = 0; j < fv.size(); j++) { 
+        fv[j] = (fv[j]-means[j])/(var[j].first - var[j].second);
+      }
+      int dim = fv.size() + 1;
+      nd.outValue = (nd.outValue - means[dim])/(var[dim].first - var[dim].second);
 		}
 	};
-	Galois::do_all(equations.begin(), equations.end(),fillGraph());
+	Galois::do_all(equations.begin(), equations.end(),fillGraph(means,variance));
 }
-typedef GaloisRuntime::PerThreadStorage<vector<double> > threadF; 
-typedef GaloisRuntime::PerThreadStorage<double> threadG;
+typedef GaloisRuntime::PerThreadStorage<vector<float> > threadF; 
+typedef GaloisRuntime::PerThreadStorage<float> threadG;
 struct Process { 
 	threadF &localGains; 
 	threadG &gainValue;
-	Process(threadF &lg, threadG &gv):localGains(lg),gainValue(gv) { 
+  int featureSize;
+	Process(threadF &lg, threadG &gv,int featureSize):
+    localGains(lg),gainValue(gv) {
+     this->featureSize = featureSize; 
 	}
 	void operator()(GNode node) { 
 		auto &nd = graph.getData(node,Galois::NONE);
-		vector <double> &localGain(*localGains.getLocal());
-		double error = 0;
-		double expectedValue = 0.0;
+		vector <float> &localGain(*localGains.getLocal());
+		float error = 0;
+		float expectedValue = 0.0;
 		for (int i = 0; i < featureSize; i++) {
 			expectedValue += globalThetas[i] * nd.featureValues[i];
 		}
 		error = (expectedValue - nd.outValue);
-		double &gain(*gainValue.getLocal());
+		float &gain(*gainValue.getLocal());
 		gain = error;
 		for (int i = 0; i < featureSize; i++) {
 			localGain[i]+= error * nd.featureValues[i];
@@ -121,56 +116,39 @@ struct Process {
 int main(int argc,char **argv) { 
 	Galois::StatManager statManager;
 	LonestarStart(argc, argv, name, desc, url);
-	readGraph(filename);
+  int featureSize,numSamples;
+	readGraph(filename,featureSize,numSamples);
 	//TODO feature scaling and mean normalization for x values and y values 
-
 	std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> dis(-1, 1);
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<float> dis(-1, 1);
 	//Initialize globalThetas
 	for ( int i = 0; i < featureSize; i++) { 
 		globalThetas[i] = dis(gen);
 	}
-	double alpha = 0.1; //something 
+	float alpha = 0.1; //something 
 	do {
 		threadF localGains; 
 		threadG gainValues;
 		for (int i = 0; i < localGains.size(); i++) { 
 			localGains.getRemote(i)->resize(featureSize);
-			double &gain(*gainValues.getRemote(i));
+			float &gain(*gainValues.getRemote(i));
 			gain = 0;
 		}
-		Galois::do_all_local(graph,Process(localGains,gainValues));
-		vector <double> globalGain(featureSize);
+		Galois::do_all_local(graph,Process(localGains,gainValues,featureSize));
+		vector <float> globalGain(featureSize);
 		for (int i = 0; i < localGains.size(); i++) { 
-			vector<double> &localGain(*localGains.getRemote(i));
+			vector<float> &localGain(*localGains.getRemote(i));
 			for (int j = 0; j < localGain.size(); j++) {
 				globalGain[j] += localGain[i];
 			}
 		}
-
 		for (int i = 0; i < featureSize; i ++) { 
 			globalThetas[i] -= alpha * globalGain[i];
 		}
-
 		//Using gainValues do:
 		//TODO specify termination condition here 
-		//Change learning rate hre if the convergence is too slow 
+		//Change learning rate here if the convergence is too slow 
 		//Change Learning rate here if the gain not monotonically decreasing
-
 	}while(true);
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
